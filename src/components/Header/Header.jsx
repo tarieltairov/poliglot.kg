b5712c90c964b5ee3bb62cb92590fc346f2647e7
tarieltairov <tarieltairov1@gmail.com>
2022-03-19 18:47:03 +0600

    whithout animate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,31 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import style from "./Header.module.scss";
 import Logo from "../../assets/new/logo.png";
 import Image from "next/image";
 import { MainMenu } from "./Menu/MainMenu";
-
-const Header = ({inView }) => {
+import { motion } from "framer-motion"
+const Header = ({ inView }) => {
   return (
     <header
-      className={`${style.mainHeader} ${
-        inView ? style.mainHeaderBlack : style.mainHeaderWhite
-      }`}
+      className={`${style.mainHeader} ${inView ? style.mainHeaderBlack : style.mainHeaderWhite
+        }`}
     >
       <div className={`container`}>
-        <div className={style.header}>
+        < motion.div className={style.header}
+          initial='hidden'
+          whileInView='visible'
+          transition={{ duration: 0.5 }}
+          variants={{
+            visible: { opacity: 1, scale: 1 },
+            hidden: { opacity: 0, scale: 0 },
+          }}
+        >
           <div className={style.logo}>
-            <Image width={75} height={75} src={Logo} className={style.logo_image}/>
+            <Image width={75} height={75} src={Logo} className={style.logo_image} />
             <h2>Ta Trains Inc</h2>
           </div>
           <MainMenu />
-        </div>
+        </ motion.div>
       </div>
     </header>
   );
