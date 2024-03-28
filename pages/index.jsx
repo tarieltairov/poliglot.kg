4e5fcf1aee3dd22a83fbc88c5a4919444a18f959
Tariel <tarieltairov1@gmail.com>
2024-03-28 19:46:58 +0300

    adaptive: Expertice

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ function Home() {
       {/* <Hero /> */}
       {/* <About /> */}
       {/* <Services /> */}
-      {/* <Expertice /> */}
+      <Expertice />
       <Solutions />
       <Slider />
       <PreFooter />
