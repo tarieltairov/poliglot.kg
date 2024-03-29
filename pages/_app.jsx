d429a1290512651f9d11e48128bcbb961b75e605
Tariel <tarieltairov1@gmail.com>
2024-03-29 13:13:14 +0300

    remove unneeded components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../src/style/global.scss";
-import "../src/components/Home/Techonolgies/animation.scss";
 import "../src/components/Header/Menu/menu-styles.scss";
 import "../src/components/components/ContentHeadTitlte/content-head-title.scss";
 import { appWithTranslation } from "next-i18next";
